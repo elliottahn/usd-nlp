a33ad1552a3e6416dcfe5b51777aa1b33ed923b4
elliottahn <seokhyun.ahn@gmail.com>
2026-05-22 23:13:26 +0900

    Bump version to 0.1.1 for SoftwareX revision

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="usd-nlp",
-    version="0.1.0",
+    version="0.1.1",
     author="Seok-Hyun Ahn",
     author_email="elliott.ahn@gist.ac.kr",
     description=("Universal Scene Description composition model "
