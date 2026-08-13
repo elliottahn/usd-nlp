99870d3e872f0af623b64af8e58679c1f4c89cc3
elliottahn <seokhyun.ahn@gmail.com>
2026-08-13 10:23:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="usd-nlp",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=3.9",
